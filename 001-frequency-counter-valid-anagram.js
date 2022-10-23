a26acdddb7133274089ed812393e96a105bc53f3
xaviguasch <xaviguasch@gmail.com>
2022-10-23 13:36:00 +0200

    added 001-frequency-counter-valid-anagram challenge

---
+++ 001-frequency-counter-valid-anagram.js
@@ -0,0 +1,21 @@
+// Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
+// validAnagram('', '') // true
+// validAnagram('aaz', 'zza') // false
+// validAnagram('anagram', 'nagaram') // true
+// validAnagram("rat","car") // false) // false
+// validAnagram('awesome', 'awesom') // false
+// validAnagram('qwerty', 'qeywrt') // true
+// validAnagram('texttwisttime', 'timetwisttext') // true
+
+const validAnagram = (str1, str2) => {
+  return
+}
+
+console.log(validAnagram('', ''))
+console.log(validAnagram('aaz', 'zza'))
+console.log(validAnagram('anagram', 'nagaram'))
+console.log(validAnagram('rat', 'car'))
+console.log(validAnagram('awesome', 'awesom'))
+console.log(validAnagram('qwerty', 'qeywrt'))
+console.log(validAnagram('texttwisttime', 'timetwisttext'))
